f805d5974d2798daeddb0cab942f28312b37e8cf
Ömer Orkun TOPTAŞ <omerorkuntoptas@gmail.com>
2022-05-04 12:08:37 +0300

    Add files via upload

---
+++ DenemeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DenemeAppApp.swift
+//  DenemeApp
+//
+//  Created by Orkun Toptaş on 2.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct DenemeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
